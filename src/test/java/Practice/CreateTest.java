6186ef800c6d242693aec0bd99e6e6e02e2d48d9
ashir <ashir@LAPTOP-HK26NAV8>
2023-01-19 17:03:07 +0530

    oijoe

--- src/test/java/Practice/CreateTest.java
+++ src/test/java/Practice/CreateTest.java
@@ -4,12 +4,7 @@ import org.testng.annotations.Test;
 
 public class CreateTest {
 
-	@Test(groups = "smoke")
-	public void create()
-	{
-		System.out.println("craeted sucesfully 3");
-	}
-	@Test(groups = "regression")
+		@Test(groups = "regression")
 	public void modify()
 	{
 		System.out.println("modified sucesfully669");
