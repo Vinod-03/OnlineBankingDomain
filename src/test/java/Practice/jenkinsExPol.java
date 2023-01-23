0eb4baaf9f80f72afe460250a65448e18179197b
vinod <vinod@VINOD>
2023-01-23 11:56:53 +0530

    pol

---
+++ src/test/java/Practice/jenkinsExPol.java
@@ -0,0 +1,18 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class jenkinsExPol {
+
+	
+	@Test(groups = "smoke")
+	public void test3()
+	{
+		System.out.println("craeted sucesfully 6");
+	}
+	@Test(groups = "regression")
+	public void test5()
+	{
+		System.out.println("modified sucesfullyh667");
+	}
+}
